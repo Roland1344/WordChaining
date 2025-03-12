76e84e853d5d336d60e5468f2e62a6bdf6978feb
Roland1344 <128683916+Roland1344@users.noreply.github.com>
2025-03-12 09:32:42 +0100

    Update controller.ts

--- src/controller.ts
+++ src/controller.ts
@@ -1,36 +1,32 @@
 import * as view from "./view.js";
 import * as model from "./model.js";
 
-const createController = () => {
-    const startGame = () => {
-        model.initializeWords(); 
-        view.renderWords(model.getCurrentWord(), model.getTargetWord());
-    };
-
-    const handleInput = (value: string) => {
-        if (model.getCurrentWord() === model.getTargetWord()) {
-            return; 
-        }
-        const valid = model.addInputValue(value);
-
-        if (valid) {
-            view.updateWordCount(model.getInputValues());
-            view.clearMiddleInput();
-            model.setCurrentWord(value);
-
-            if (model.getCurrentWord() === model.getTargetWord()) {
-                view.disableMiddleInput();
-            }
-        } else {
-            alert("Érvénytelen szó!\n Kérlek, adj meg egy három karakterből álló szót.");
-            view.clearMiddleInput(); 
-        }
-    };
-
-    view.init({
-        startGame,
-        handleInput,
-    });
+const startGame = () => {
+    model.initializeWords();
+    view.renderWords(model.getCurrentWord(), model.getTargetWord());
 };
 
-createController();
+const handleInput = (value: string) => {
+    if (model.getCurrentWord() === model.getTargetWord()) {
+        return;
+    }
+    const valid = model.addInputValue(value);
+
+    if (valid) {
+        view.updateWordCount(model.getInputValues());
+        view.clearMiddleInput();
+        model.setCurrentWord(value);
+
+        if (model.getCurrentWord() === model.getTargetWord()) {
+            view.disableMiddleInput();
+        }
+    } else {
+        alert("Érvénytelen szó!\n Kérlek, adj meg egy három karakterből álló szót.");
+        view.clearMiddleInput();
+    }
+};
+
+view.init({
+    startGame,
+    handleInput,
+});
