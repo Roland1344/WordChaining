9322a41a471ef178eb22f333c3d045fa29b5dde7
R4bbitzzzz <agostonpalatinus@gmail.com>
2025-03-12 09:22:45 +0100

    f

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import * as view from './view'; 
+import * as model from './model';
+
+const createController = () => {
+    
+    const startGame = () => {
+        model.initializeWords(); 
+        view.renderWords(model.getCurrentWord(), model.getTargetWord());
+    };
+    const handleInput = (value: string) => {
+        if (model.getCurrentWord() === model.getTargetWord()) {
+            return;
+        }
+        model.addInputValue(value);
+        view.updateWordCount(model.getInputValues());
+        if (model.getCurrentWord() === model.getTargetWord()) {
+            view.disableMiddleInput();
+        }
+    };
+
+    view.init({
+        startGame,
+        handleInput,
+    });
+};
+
+createController();
