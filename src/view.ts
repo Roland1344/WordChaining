9a12698d7798bcba0127398afadfef1706af55a6
Roland1344 <128683916+Roland1344@users.noreply.github.com>
2025-03-12 09:33:15 +0100

    Update view.ts

--- src/view.ts
+++ src/view.ts
@@ -1,12 +1,10 @@
-export { init, renderWords, updateWordCount, disableMiddleInput, clearMiddleInput };
-
 const startButton = document.getElementById("start") as HTMLButtonElement;
 const leftInput = document.getElementById("left") as HTMLInputElement;
 const middleInput = document.getElementById("middle") as HTMLInputElement;
 const rightInput = document.getElementById("right") as HTMLInputElement;
 const wordCount = document.getElementById("wordCount") as HTMLElement;
 
-const init = (controller: any) => {
+export const init = (controller: any) => {
     startButton.addEventListener("click", () => controller.startGame());
     middleInput.addEventListener("keypress", (event) => {
         if (event.key === "Enter") {
@@ -15,22 +13,22 @@ const init = (controller: any) => {
     });
 };
 
-const renderWords = (startWord: string, endWord: string) => {
+export const renderWords = (startWord: string, endWord: string) => {
     leftInput.value = startWord;
     rightInput.value = endWord;
     middleInput.value = "";
     wordCount.textContent = `Felhasznált szavak száma: 0`;
 };
 
-const updateWordCount = (inputValues: string[]) => {
+export const updateWordCount = (inputValues: string[]) => {
     wordCount.textContent = `Felhasznált szavak száma: ${inputValues.length}`;
 };
 
-const disableMiddleInput = () => {
+export const disableMiddleInput = () => {
     middleInput.disabled = true;
 };
 
-const clearMiddleInput = () => {
+export const clearMiddleInput = () => {
     middleInput.value = "";
     middleInput.focus();
 };
